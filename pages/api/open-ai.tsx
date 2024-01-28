434cfb5acfd53393a16bdfcc66c48845f71b6f39
nheek <nickjameshipol@gmail.com>
2024-01-28 13:31:41 +0100

    added pages/api/open-ai.tsx

---
+++ pages/api/open-ai.tsx
@@ -0,0 +1,39 @@
+const fetchChatGPT = async (messages) => {
+    try {
+      // Retrieve the OpenAI API key from environment variables
+      const apiKey = process.env.NEXT_PUBLIC_OPEN_AI_API_KEY; 
+  
+      // Set the API URL for the Chat API
+      const apiUrl = 'https://api.openai.com/v1/chat/completions';
+  
+      // Make a POST request to the OpenAI API with the provided messages
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+          'OpenAI-Beta': 'assistants=v1',
+        },
+        body: JSON.stringify({
+          model: 'gpt-3.5-turbo-1106',
+          messages,
+        }),
+      });
+  
+      // Check if the API request was successful
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+  
+      // Parse the response JSON and extract the generated content
+      const data = await response.json();
+      return { role: 'system', content: data.choices[0].message.content.trim()};
+    } catch (error) {
+      // Handle any errors that occur during the API request
+      console.error('Error fetching from ChatGPT API:', error);
+      return null;
+    }
+  };
+  
+  // Export the function for use in other parts of the application
+  export default fetchChatGPT;
